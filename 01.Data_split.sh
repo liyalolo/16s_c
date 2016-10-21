13caebe1d4ea80875911fe1fd9d09a2626b50e47
liyalolo <187072414@qq.com>
2016-10-21 22:43:52 +0800

    Create 01.Data_split.sh

---
+++ 01.Data_split.sh
@@ -0,0 +1,5 @@
+
+/Disk03/Project/Micro/liyanli/A680_160712watMG_20160906/16s/16s_49samples_atlantic/FLASH-1.2.11/FLASH-1.2.11/flash  /Disk03/Project/Micro/liyanli/A680_160712watMG_20160906/16s/16s_49samples_atlantic/raw/16S_49samples_rawdata_zhoujin_qinghua/16S-ZJ01_CAACTGT-GACGACC_L001_R1_001.fastq  /Disk03/Project/Micro/liyanli/A680_160712watMG_20160906/16s/16s_49samples_atlantic/raw/16S_49samples_rawdata_zhoujin_qinghua/16S-ZJ01_CAACTGT-GACGACC_L001_R2_001.fastq -d /Disk03/Project/Micro/liyanli/A680_160712watMG_20160906/16s/16s_49samples_atlantic/01.Data_split/NA1 2>/Disk03/Project/Micro/liyanli/A680_160712watMG_20160906/16s/16s_49samples_atlantic/01.Data_split/NA1/NA1.flash.log
+
+
+
